ff43f20de7eceaaa5e7f59263e22f55864457b13
Angel Santana <angel.d.santana00@gmail.com>
2022-02-15 16:01:24 -0600

    exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,33 @@
+//class - defining the class
+// HelloWorld is the name of the class and it matches the name of the .java file
 public class HelloWorld {
+
+    // method - similar to a function in javascript
+    // main method -> specific method in java that acts as our entry point when running java
+    // (similar to starting point in a board game or a front door to a house)
+
+    public static void main(String[] args) {
+
+        // public - pinned for later
+        // static - pinned for later
+        // void - for now, it means this method doesn't expect anything to be returned
+        // main - name of our method, main is keyword that is going to be search as the entry point
+        // String[] args - passing in a string array named args
+
+        // print out "hello world" to the console
+        System.out.println("Hello World");
+        System.out.print("Hello World!!!!!!!!");
+
+        int myFavoriteNumber = 21;
+        System.out.print("\nmyFavoriteNumber");
+
+        String MyString = "Hello";
+        System.out.print("\nMyString");
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+    }
+
 }
+
