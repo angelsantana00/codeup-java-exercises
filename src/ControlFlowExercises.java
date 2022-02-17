35e7e57b18b5c33e71543a7fe2a74cc417a8942c
Angel Santana <angel.d.santana00@gmail.com>
2022-02-17 16:21:43 -0600

    exercise number 3 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
-    public static void main ( String[] args) {
+    public static void main(String[] args) {
 
         // -------------- 1A. ------------------- //
 //        int i = 5;
@@ -28,25 +30,51 @@ public class ControlFlowExercises {
 //                i = (long) Math.pow(i,2);
 //            } while (i < 1000000);
 
-            // ----------------- 2. ----------------- //
-        String mult3 = "Fizz";
-        String mult5 = "Buzz";
-        String mult3and5 = "Fizzbuzz";
+        // ----------------- 2. ----------------- //
+//        String mult3 = "Fizz";
+//        String mult5 = "Buzz";
+//        String mult3and5 = "Fizzbuzz";
+//
+//        for (int i = 1; i <101 ;i++ ) {
+//            if ((i % 3==0) && (i % 5==0)){
+//                System.out.println(mult3and5+" ");
+//            }
+//            else if (i%5==0){
+//                System.out.println(mult5+ "");
+//            }
+//            else if ((i %3==0)){
+//                System.out.println(mult3 + "");
+//            }
+//            else
+//                System.out.println(i);
+//        }
 
-        for (int i = 1; i <101 ;i++ ) {
-            if ((i % 3==0) && (i % 5==0)){
-                System.out.println(mult3and5+" ");
+        // ------------------ 3. ----------------- //
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+
+        while (choice.equalsIgnoreCase("y")) {
+            // get the input from the user
+            System.out.println("What number would you like to go up to? ");
+            int integerNext = sc.nextInt();
+
+            //What I want to present and how it'll look + function
+            System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");
+            System.out.println("======" + "  " + "======" + "  " + "======");
+
+            for (int i = 1; i < integerNext; i++) {
+                i = integerNext;
+                int numberSquared = (int) Math.pow(i, 2);
+                int numberCubed = (int) Math.pow(i, 3);
+
+                String message = "\n" + i + "    |   " + numberSquared + "    |   " + numberCubed;
+
+                System.out.println(message);
+                System.out.println();
+
             }
-            else if (i%5==0){
-                System.out.println(mult5+ "");
-            }
-            else if ((i %3==0)){
-                System.out.println(mult3 + "");
-            }
-            else
-                System.out.println(i);
+
+
         }
-
-
     }
 }
