7206aa8f4fb30985d97b8bcc0c1c964000c16c1f
Angel Santana <angel.d.santana00@gmail.com>
2022-02-21 12:27:12 -0600

    integer method complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,7 +1,16 @@
+import java.util.Locale;
 import java.util.Scanner;
 
-public class MethodsExercises {
 
+public class MethodsExercises {
+    public static void main(String[] args) {
+//        System.out.println(AdditionMethod(1, 6));
+//        System.out.println(SubtractionMethod(10, 5));
+//        System.out.println(MultiplicationMethod(10, 6));
+//        System.out.println(DivisionMethod(30, 7));
+//        System.out.println(FindModOfTwo(100, 30));
+        System.out.println(getInteger(2, 10));
+    }
     //static Scanner in = new Scanner(System.in);
 
     // <---------------------- 1. ---------------------> //
@@ -27,6 +36,8 @@ public class MethodsExercises {
 //    public static int FindModOfTwo(int number, int numberTwo) {
 //        return (number % numberTwo);
 //    }
+
+    // <-------------------- 2. -------------------> //
     public static int getInteger(int min, int max) {
 
         Scanner in = new Scanner(System.in);
@@ -35,23 +46,14 @@ public class MethodsExercises {
         System.out.print("Enter a number between " + min + " and " + max + ":");
         int input = in.nextInt();
         if (input < min || input > max) {
-            System.out.println("Input  not Accepted");
+            System.out.println("Input not Accepted");
             return getInteger(min, max);
-
         }
-        //   } while (input < min || input > max);
         return input;
     }
 
-    public static void main(String[] args) {
-//        System.out.println(AdditionMethod(1, 6));
-//        System.out.println(SubtractionMethod(10, 5));
-//        System.out.println(MultiplicationMethod(10, 6));
-//        System.out.println(DivisionMethod(30, 7));
-//        System.out.println(FindModOfTwo(100, 30));
-        getInteger(2, 10);
 
 
     }
 
-}
+
