c718d5e9676cd25f2f8f6981f1807a7e907cbbc8
Angel Santana <angel.d.santana00@gmail.com>
2022-02-23 12:10:13 -0600

    input not complete

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,45 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+
+    public Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+
+
+    }
+
+//    public String getString() {
+////        System.out.println("String here: ");
+//        String userString = scanner.nextLine();
+//        return userString;
+//    }
+//
+//    public boolean yesNo() {
+////        System.out.println("Select (y/n):");
+//        String userAnswer = scanner.next();
+//        return userAnswer.equals("y");
+//    }
+
+    public int getInt(int min, int max) {
+        System.out.println("Enter Interger: ");
+        int userInt;
+        try {
+            userInt = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Out of Range");
+            return getInt(min, max);
+        }
+        if (userInt < min || userInt > max) {
+            System.out.println("Out of Range");
+            return getInt(min, max);
+        } else {
+            System.out.println("In Range");
+            return userInt;
+        }
+    }
+
 }
