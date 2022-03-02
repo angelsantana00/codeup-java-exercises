4c73da01cb811500c620f8cd3989c0065d632ede
Angel Santana <angel.d.santana00@gmail.com>
2022-03-02 12:27:32 -0600

    created abstract quad

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,17 @@
 package shapes;
 
-abstract class Quadrilateral {
+abstract class Quadrilateral extends Shape implements Measurable {
+
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength () {
+        return length;
+    }
+
 }
