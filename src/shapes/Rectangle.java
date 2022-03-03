8ed2e2b16b2297c19e9fec6a035dbd1c18913829
Angel Santana <angel.d.santana00@gmail.com>
2022-03-03 10:07:22 -0600

    added extension to square.java

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,5 +1,29 @@
-//package shapes;
-//
+package shapes;
+
+public class Rectangle extends Quadrilateral implements Measurable {
+    public Rectangle(double length, double width) {
+        super(length, width);
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (this.length + this.width);
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+}
+
+
+
 //public class Rectangle {
 //
 //    protected int length;
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,3 +1,31 @@
+package shapes;
+
+class Square extends Quadrilateral {
+    public Square(double side){
+        super (side, side);
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPerimeter() {
+        return this.length * 4;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+}
+
+
+
+
 //package shapes;
 //
 //public class Square extends Rectangle {
