6c8fcf979de5789cfb5358881197a0765e5b8504
Angel Santana <angel.d.santana00@gmail.com>
2022-03-15 10:37:48 -0500

    autosave 2

--- examples.sql
+++ examples.sql
@@ -1,3 +1,8 @@
 Make sure the MySQL server is running on your laptop.
 
 mysql.server start
+
+SELECT * FROM mysql.user
+SELECT user, host FROM mysql.user
+SELECT * FROM mysql.help_topic
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic
