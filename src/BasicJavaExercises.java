bcdab366fe631c8ae8f3a5c6d7b75ee627519db0
Angel Santana <angel.d.santana00@gmail.com>
2022-03-27 21:53:10 -0500

    upload

--- src/BasicJavaExercises.java
+++ src/BasicJavaExercises.java
@@ -5,6 +5,7 @@ class BasicJavaExercises {
     //TODO: 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
     static class Exercise1 {
         public static void main(String[] args) {
+
             System.out.println("Hello, \nAlexandra Abramov");
         }
     }
@@ -12,6 +13,7 @@ class BasicJavaExercises {
     //TODO: 2. Write a Java program to print the sum of two numbers.
     static class Exercise2 {
         public static void main(String[] args) {
+
             System.out.println(24 + 26);
         }
     }
@@ -19,6 +21,7 @@ class BasicJavaExercises {
     //TODO: 3. Write a Java program to divide two numbers and print on the screen.
     static class Exercise3 {
         public static void main(String[] args) {
+
             System.out.println(50 / 3);
         }
     }
@@ -26,6 +29,7 @@ class BasicJavaExercises {
     //TODO: 4. Write a Java program to print the result of the following operations.
     static class Exercise4 {
         public static void main(String[] args) {
+
             System.out.println(-5 + 8 * 6);
             System.out.println((55 + 9) % 9);
             System.out.println(20 + -3 * 5 / 8);
@@ -36,6 +40,7 @@ class BasicJavaExercises {
     //TODO: 5. Write a Java program that takes two numbers as input and display the product of two numbers.
     static class Exercise5 {
         public static void main(String[] args) {
+
             Scanner in = new Scanner(System.in);
 
             System.out.print("Input first number: ");
@@ -51,6 +56,7 @@ class BasicJavaExercises {
     //TODO: Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
     static class Exercise6 {
         public static void main(String[] args) {
+
             Scanner in = new Scanner(System.in);
 
             System.out.print("Input Number: ");
@@ -80,6 +86,7 @@ class BasicJavaExercises {
     //TODO: 7. Write a Java program that takes a number as input and prints its multiplication table upto 10.
     static class Exercise7 {
         public static void main(String[] args) {
+
             Scanner in = new Scanner(System.in);
 
             System.out.print("Input a number: ");
@@ -95,6 +102,7 @@ class BasicJavaExercises {
     //TODO: 8. Write a Java program to display the following pattern.
     static class Exercise8 {
         public static void main(String[] args) {
+
             System.out.println("    J    a   v      v   a\n" +
                                "    J   a  a   v   v   a a\n" +
                                "J   J  aaaaaa   V V   aaaaaa\n" +
@@ -105,6 +113,7 @@ class BasicJavaExercises {
     //TODO: 9. Write a Java program to compute the specified expressions and print the output.
     static class Exercise9 {
         public static void main(String[] args) {
+
             System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
         }
     }
@@ -112,13 +121,49 @@ class BasicJavaExercises {
     //TODO: 10. Write a Java program to compute a specified formula.
     static class Exercise10 {
         public static void main(String[] args) {
+
             double result = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
             System.out.println(result);
         }
+    }
+
+    //TODO: 11. Write a Java program to print the area and perimeter of a circle.
+    static class Exercise11 {
+
+        private static final double radius = 10;
+        public static void main(String[] args) {
+
+            double perimeter = 2 * Math.PI * radius;
+            double area = Math.PI * radius * radius;
+
+            System.out.println("::::::::: Circle Stats ::::::::::");
+            System.out.println("Perimeter is = " + perimeter);
+            System.out.println("Area is = " + area);
+            System.out.println(":::::::::::::::::::::::::::::::::");
+        }
+    }
+
+    //TODO: BONUS . Write a java program to print input from user to Lower and Uppercase
+    static class ExerciseBonus {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Type something: ");
+            String line = in.nextLine();
+            line = line.toLowerCase();
+
+            System.out.println(line);
+        }
+    }
+
+
+    public static String capitalOrLowerCase(String s, String option) {
+//
     }
 
 
 
 
 
-}
+
+
+    }
