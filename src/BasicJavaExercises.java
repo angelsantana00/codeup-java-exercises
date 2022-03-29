417e727a0cc776fa4d748c021916732da37eb74e
Angel Santana <angel.d.santana00@gmail.com>
2022-03-29 11:57:05 -0500

    easy peasy

--- src/BasicJavaExercises.java
+++ src/BasicJavaExercises.java
@@ -155,10 +155,34 @@ class BasicJavaExercises {
         }
     }
 
+    //TODO: Write a java program to print out the average of 5 numbers inputted by the user
+    static class Exercise12 {
 
-    public static String capitalOrLowerCase(String s, String option) {
-//
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Input first number: ");
+            int num1 = in.nextInt();
+
+            System.out.print("Input second number: ");
+            int num2 = in.nextInt();
+
+            System.out.print("Input third number: ");
+            int num3 = in.nextInt();
+
+            System.out.print("Input fourth number: ");
+            int num4 = in.nextInt();
+
+            System.out.print("Enter fifth number: ");
+            int num5 = in.nextInt();
+
+
+            System.out.println("Average of five numbers is: " +
+                    (num1 + num2 + num3 + num4 + num5) / 5);
+        }
     }
+
+
 
 
 
