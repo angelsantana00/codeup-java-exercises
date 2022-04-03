4e4294b1272544322356329fcfc2b6b760c46964
Angel <angel.d.santana00@gmail.com>
2022-04-03 11:07:23 -0500

    EX15 done

--- src/BasicJavaExercises.java
+++ src/BasicJavaExercises.java
@@ -223,8 +223,23 @@ class BasicJavaExercises {
 
     }
 
-    //exercise 15 next
+    //TODO: Write a Java program to swap two variables.
 
+    static class Exercise15 {
+
+        public static void main(String[] args) {
+
+            int a, b, temp;
+            a = 15;
+            b = 27;
+            System.out.println("Before swapping : a, b = "+a+", "+ + b);
+            temp = a;
+            a = b;
+            b = temp;
+            System.out.println("After swapping : a, b = "+a+", "+ + b);
+        }
+
+    }
 
 
 
