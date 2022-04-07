c3c8d2a8304eea0769ab56b747145de4f29940f1
Angel Santana <angel.d.santana00@gmail.com>
2022-04-07 17:03:31 -0500

    reversed a string

--- src/BasicJavaExercises.java
+++ src/BasicJavaExercises.java
@@ -345,7 +345,7 @@ class BasicJavaExercises {
         }
     }
 
-    //TODO:
+    //TODO:Write a Java program to convert a decimal number to binary number.
     static class Exercise19 {
         public static void main(String args[])
         {
@@ -372,6 +372,22 @@ class BasicJavaExercises {
             System.out.print("\n");
         }
     }
+
+    //TODO: Write a Java program to reverse a string.
+
+    static class Exercise27{
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Input a string: ");
+            char[] letters = scanner.nextLine().toCharArray();
+            System.out.print("Reverse string: ");
+            for (int i = letters.length - 1; i >= 0; i--) {
+                System.out.print(letters[i]);
+            }
+            System.out.print("\n");
+        }
+    }
+
 
 
 
